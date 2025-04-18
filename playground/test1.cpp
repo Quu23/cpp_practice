60443f6d20111931e9e4738011e79783a27489d8
Quu23 <kiyu@kaz.bbiq.jp>
2025-04-18 22:21:42 +0900

    環境構築をしてみた

---
+++ playground/test1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    std::cout << "Hello World"s << "ass";
+    std::cout << "ai"s+"ueo"s;
+
+    std::cout << 3.14-2;
+    std::cout << 3.33*5+1 << "\n"s << 22;
+}
